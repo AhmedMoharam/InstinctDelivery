e70678fe2034dac0db74faec69dab93ed9e53123
AhmedMoharam <33280932+AhmedMoharam@users.noreply.github.com>
2021-01-11 23:50:01 +0200

    creating cpp_interface, implementing it inside BP actor, calling interface function from game_mode.cpp after construction the BP actor

--- Source/InstinctDelivery/InstinctDeliveryGameMode.cpp
+++ Source/InstinctDelivery/InstinctDeliveryGameMode.cpp
@@ -3,6 +3,7 @@
 #include "InstinctDeliveryGameMode.h"
 #include "InstinctDeliveryBall.h"
 #include "Engine/World.h"
+#include "level_cell_interface.h"
 
 AInstinctDeliveryGameMode::AInstinctDeliveryGameMode()
 {
@@ -15,5 +16,11 @@ void AInstinctDeliveryGameMode::BeginPlay()
 {
 	FVector location(0,0,0);
 	FRotator rotation(0,0,0);
-	GetWorld()->SpawnActor<AActor>(cell_class_name, location, rotation);
+	AActor* cell = GetWorld()->SpawnActor<AActor>(cell_class_name, location, rotation);
+	if (cell->GetClass()->ImplementsInterface(Ulevel_cell_interface::StaticClass())) {
+		UE_LOG(LogTemp, Warning, TEXT("SUCESS!"));
+		int val = Ilevel_cell_interface::Execute_getValue(cell);
+		UE_LOG(LogTemp, Warning, TEXT("Value is %d !"), val);
+	}
+	
 }
---
+++ Source/InstinctDelivery/level_cell_interface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "level_cell_interface.h"
+
+// Add default functionality here for any Ilevel_cell_interface functions that are not pure virtual.
